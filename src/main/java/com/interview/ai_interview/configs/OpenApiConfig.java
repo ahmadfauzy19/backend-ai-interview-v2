771fc994738e84bf6e671bf913493d29c56c7bf0
Ahmad Fauzy <ahmad.fauzy@rollingglory.com>
2026-03-04 12:33:00 +0700

    fix :: open api

--- src/main/java/com/interview/ai_interview/configs/OpenApiConfig.java
+++ src/main/java/com/interview/ai_interview/configs/OpenApiConfig.java
@@ -1,30 +1,53 @@
 package com.interview.ai_interview.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 @Configuration
-@Profile("prod")
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+    public OpenAPI customOpenAPI(Environment env) {
+
+        final String securitySchemeName = "bearerAuth";
+
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("AI Interview API")
                         .version("v1")
                 )
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
 
-                // Production Server
-                .servers(List.of(
-                        new Server().url("https://ai-interview.jiwamu.de")
-                ));
+        // hanya aktif saat prod
+        if (List.of(env.getActiveProfiles()).contains("prod")) {
+            openAPI.setServers(
+                    List.of(new Server().url("https://ai-interview.jiwamu.de"))
+            );
+        }
+
+        return openAPI;
     }
 }
